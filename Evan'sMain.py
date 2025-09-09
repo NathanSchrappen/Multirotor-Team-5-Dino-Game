65c57d1d1e194ad2f2ab7854d9de3b1832a55970
Evan <ecrb29@umsystem.edu>
2025-09-09 17:43:18 -0500

    kk

--- Evan'sMain.py
+++ Evan'sMain.py
@@ -16,12 +16,14 @@ def calculate_jump(enter : float, exit : float) -> float:
     dinoDistance = 1260 - 665
     hitTime = dinoDistance/speed #time until hit
 
-    jumpTime = hitTime - 0.5 #edit second number accordingly as this is the delay it takes the dino to jump
+    jumpTime = hitTime - 0.1 #edit second number accordingly as this is the delay it takes the dino to jump
+
+    print(jumpTime)
 
     return max(jumpTime, 0)
     
 #getting in game and starting
-webbrowser.get("C:/Program Files/Google/Chrome/Application/chrome.exe %s").open("chrome://dino", new=2)
+webbrowser.get("C:/Program Files/Google/Chrome/Application/chrome.exe %s").open("chrome://dino", new=1)
 time.sleep(2)
 keyboard.write("Chrome://dino")
 keyboard.press_and_release('enter')
@@ -40,7 +42,8 @@ while event.is_set() == 0:
         time_stack1.append(time.time())
     elif len(time_stack1)>len(time_stack2):
         time_stack2.append(time.time())
-        threading.Timer(calculate_jump(time_stack1.pop(0),time_stack2.pop(0)), lambda: keyboard.press_and_release("Space")).start()
+        delay = calculate_jump(time_stack1.pop(0),time_stack2.pop(0))
+        threading.Timer(delay, keyboard.press_and_release("Space")).start()
 
     time.sleep(0.001)
 
