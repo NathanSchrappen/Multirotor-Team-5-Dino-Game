3cab7341afcb6201340cbaf3b9dfd064dcf19271
Evan <ecrb29@umsystem.edu>
2025-09-08 19:46:39 -0500

    Evan's stuff one

--- Evan'sMain.py
+++ Evan'sMain.py
@@ -1,4 +1,31 @@
 import keyboard
 import webbrowser
+import time
+import threading
+import getpixelcolor
 
-webbrowser.get()
+event = threading.Event()
+
+def complete():
+    event.set()
+    
+#getting in game and starting
+webbrowser.get("C:/Program Files/Google/Chrome/Application/chrome.exe %s").open("chrome://dino", new=2)
+time.sleep(2)
+keyboard.write("Chrome://dino")
+keyboard.press_and_release('enter')
+time.sleep(1) 
+keyboard.press_and_release('space')
+        
+
+keyboard.add_hotkey("ctrl+c", complete)
+while event.is_set() == 0:
+    keyboard.press_and_release("space")
+    #colors = getpixelcolor.area()
+    time.sleep(1)
+
+
+
+
+
+
