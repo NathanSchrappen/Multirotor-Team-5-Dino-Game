046c07ce9797ec419916cbbdbf13f24ba62bfdd2
Evan <ecrb29@umsystem.edu>
2025-09-09 17:12:55 -0500

    made some updates for timing function

--- Evan'sMain.py
+++ Evan'sMain.py
@@ -6,8 +6,19 @@ import getpixelcolor
 
 event = threading.Event()
 
+#Function will stop the program from running
 def complete():
     event.set()
+
+#Function that will calculate the time when the dino needs to jump
+def calculate_jump(enter : float, exit : float) -> float:
+    speed = 25/(exit-enter) #pps
+    dinoDistance = 1260 - 665
+    hitTime = dinoDistance/speed #time until hit
+
+    jumpTime = hitTime - 0.5 #edit second number accordingly as this is the delay it takes the dino to jump
+
+    return max(jumpTime, 0)
     
 #getting in game and starting
 webbrowser.get("C:/Program Files/Google/Chrome/Application/chrome.exe %s").open("chrome://dino", new=2)
@@ -17,12 +28,21 @@ keyboard.press_and_release('enter')
 time.sleep(1) 
 keyboard.press_and_release('space')
         
-
+#Game logic
+time_stack1 = []
+time_stack2 = []
 keyboard.add_hotkey("ctrl+c", complete)
 while event.is_set() == 0:
-    keyboard.press_and_release("space")
-    #colors = getpixelcolor.area()
-    time.sleep(1)
+    colors = getpixelcolor.average(1260,300,25,25)
+    avg = (colors[0] + colors[1] + colors[2])/3
+
+    if (avg > 40):
+        time_stack1.append(time.time())
+    elif len(time_stack1)>len(time_stack2):
+        time_stack2.append(time.time())
+        threading.Timer(calculate_jump(time_stack1.pop(0),time_stack2.pop(0)), lambda: keyboard.press_and_release("Space")).start()
+
+    time.sleep(0.001)
 
 
 
